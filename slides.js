87d4e0e9e0fcfc4c40f899421a107f2b339e9f0a
Nagesh <nageshsturup@gmail.com>
2022-07-13 20:01:09 +0530

    adding all files

---
+++ slides.js
@@ -0,0 +1,41 @@
+//   document.addEventListener("DOMContentLoaded", function (event) {
+
+// var prevLink = document.getElementsByClassName('prev')[0];
+// var nextLink = document.getElementsByClassName('next')[0];
+// alert(prevLink.innerHTML);
+// prevLink.addEventListener("click", function () {
+//     plusSlides(-1)
+// })
+
+// nextLink.addEventListener("click", function () {
+//     plusSlides(1)
+// })
+// let slideIndex = 1;
+// showSlides(slideIndex);
+
+    
+
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+// }
+
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//     let i;
+//     let slides = document.getElementsByClassName("mySlides");
+
+//     if (n > slides.length) {
+//         slideIndex = 1
+//     }
+//     if (n < 1) {
+//         slideIndex = slides.length
+//     }
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+
+// }
+// })
